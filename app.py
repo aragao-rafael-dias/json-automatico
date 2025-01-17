35218ffc62a240c2387dd2ea9395dde711ba2a29
Rafael <rafaeldiasaragao@gmail.com>
2025-01-17 11:46:38 -0300

    modified:   app.py 	new file:   procfile

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import json
+import os
 
 app = Flask(__name__)
 
@@ -51,4 +52,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
